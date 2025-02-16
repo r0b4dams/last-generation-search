2b726dac02311c38bde1edd6f52c9787dc1ebbfb
Robert Adams <r0b4dams@gmail.com>
2025-02-16 13:02:48 -0800

    Rename file

---
+++ src/letzegensuchen/user_agent.py
@@ -0,0 +1,53 @@
+win7_chrome = " ".join(
+    [
+        "Mozilla/5.0",
+        "(Windows NT 6.1; WOW64)",
+        "AppleWebKit/537.36",
+        "(KHTML, like Gecko)",
+        "Chrome/47.0.2526.111",
+        "Safari/537.36",
+    ]
+)
+
+win10_edge = " ".join(
+    [
+        "Mozilla/5.0",
+        "(Windows NT 10.0; Win64; x64)",
+        "AppleWebKit/537.36",
+        "(KHTML, like Gecko)",
+        "Chrome/42.0.2311.135",
+        "Safari/537.36",
+        "Edge/12.246",
+    ]
+)
+
+chromeos_chrome = " ".join(
+    [
+        "Mozilla/5.0",
+        "(X11; CrOS x86_64 8172.45.0)",
+        "AppleWebKit/537.36",
+        "(KHTML, like Gecko) ",
+        "Chrome/51.0.2704.64",
+        "Safari/537.36",
+    ]
+)
+
+macos_safari = " ".join(
+    [
+        "Mozilla/5.0",
+        "(Macintosh; Intel Mac OS X 10_11_2)",
+        "AppleWebKit/601.3.9",
+        "(KHTML, like Gecko)",
+        "Version/9.0.2",
+        "Safari/601.3.9",
+    ]
+)
+
+linux_firefox = " ".join(
+    [
+        "Mozilla/5.0",
+        "(X11; Ubuntu; Linux x86_64; rv:15.0)",
+        "Gecko/20100101",
+        "Firefox/15.0.1",
+    ]
+)
