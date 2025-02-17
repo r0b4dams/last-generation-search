0a9f1b7219580ed793f58c3d2da6774a76cac515
Robert Adams <r0b4dams@gmail.com>
2025-02-17 14:14:57 -0800

    Add search urls for reference

--- src/letztegensuchen/config.py
+++ src/letztegensuchen/config.py
@@ -1,1 +1,6 @@
 TAZ_BASE_URL = "https://taz.de"
+
+# Last Generation
+'https://taz.de/!s="Letzte+Generation"&isWochenende=1/'
+# Fridays for Future
+'https://taz.de/Artikel-mit-Fridays-for-Future/!s="Fridays+for+Future"&isWochenende=1/'
