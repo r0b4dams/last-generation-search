3ff77b129c50ee1819a72a5609ae133bb0cf462d
Robert Adams <r0b4dams@gmail.com>
2025-02-19 21:50:45 -0800

    Add build_search_url helper

--- src/narrative_futures/sites/taz/__init__.py
+++ src/narrative_futures/sites/taz/__init__.py
@@ -1,1 +1,25 @@
-TAZ_BASE_URL = ""
+import urllib.parse
+
+
+from narrative_futures.utils import date
+
+TAZ_BASE_URL = "https://taz.de"
+
+
+def build_search_url(query: str, weekend_only: bool, start_date: str):
+    normalized_query = query.strip().replace(" ", "+")
+
+    if not normalized_query:
+        raise Exception("invalid query")
+
+    path = [f'!s="{normalized_query}"']
+
+    if weekend_only:
+        path.append("isWochenende=1")
+
+    if start_date:
+        if not date.is_valid(start_date):
+            raise Exception("invalid date format:", start_date)
+        path.append("eTagAb=" + start_date)
+
+    return urllib.parse.urljoin(TAZ_BASE_URL, "&".join(path))
