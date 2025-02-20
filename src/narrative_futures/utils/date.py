85a6ffa3fef71ff158a1a44c842602e6318c4ee7
Robert Adams <r0b4dams@gmail.com>
2025-02-19 21:50:13 -0800

    Return bool if date valid

--- src/narrative_futures/utils/date.py
+++ src/narrative_futures/utils/date.py
@@ -3,6 +3,7 @@ from datetime import date
 
 def is_valid(date_str: str):
     try:
-        return date.fromisoformat(date_str)
+        date.fromisoformat(date_str)
+        return True
     except ValueError:
         return False
